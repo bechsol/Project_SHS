b50f87a21c52afc181be18c1414136e738041794
Benedikt Solder <besol@web.de>
2024-05-17 15:25:43 +0200

    Entered Dialogue plus Select Window

--- map.py
+++ map.py
@@ -59,8 +59,9 @@ class Scene:
         self.filter1 = Filter((WINDOW_WIDTH, WINDOW_HEIGHT), (0, 0, 0), speed=1)
         self.filter1.enabled = False
 
-        self.filter2 = Filter((WINDOW_WIDTH, WINDOW_HEIGHT), (0, 0, 0), speed=0.05)
-        self.filter2.enabled = False
+        self.filter2 = Filter((WINDOW_WIDTH, WINDOW_HEIGHT), (0, 0, 0), speed=0.01)
+        self.filter2.enabled = True
+        self.filter2.disable()
 
         # For displaying text on the screen
         self.font = pygame.font.Font("AUGUSTUS.ttf", 20)   # credit: Manfred Klein, https://www.dafont.com/fr/augustus.font
@@ -69,7 +70,7 @@ class Scene:
         self.display_text = False
         self.text_to_display = "Error: Text not set!"
         
-        self.enable_dialogue = False
+        self.enable_dialogue = True
         self.dialogue_1 = [
             "Aie, ma tête... ",
             "Mais... Mais où je suis moi?",
@@ -84,8 +85,50 @@ class Scene:
             "Une ruse malicieuse... Ou bien une trahison écoeurante! Transformer une offrande en arme, quelle immondice.",
             "Tout est si flou, je ne sais plus quoi croire... Il faudrait que je continue de chercher."
         ]
-        self.current_dialogue = self.dialogue_2
+        self.dialogue_3 = [
+            "Qu'est-ce que c'est que ça ? Un bébé ? Qu'est-ce qu'un bébé pouvait bien faire là ? "
+        ]
+        self.dialogue_4 = [
+            "Ça me revient. Ulysse nous a prévenu qu'il était dangereux de laisser le fils d'Hector vivre. On pourrait dire que c'était nécessaire.",
+            "Mais quand bien même... tuer un bébé, c'est barbare, peu importe qui est son père. Pauvre Andromaque.",
+            "Andromaque... qu'est-il arrivé à elle et aux autres? Argh ma tête... Il me semble que le campement était dressé un peu plus loin, je devrais aller y jeter un coup d'oeil."
+        ]
+        self.dialogue_5 = [
+            "Mais attends, c'est ma tente celle-là ? Ou bien celle-ci ? Ou bien ai-je vraiment logé ici ?",
+            "Nous sommes les Troyennes, à toi de retrouver quel personnage nous a arraché à notre peuple, et nous fit sienne à titre de \"récompense particulière\".",
+            "Polyxène : J'ai péri égorgée sur le tombeau d\'Achille, offerte au mort sans vie.",
+            "Andromaque : J'ai assisté à la mise à mort de mon fils, laissant sa dépouille à ma mère, forcée de suivre Néoptolème.",
+            "Cassandre : Prétresse d'Apollon et fille de Priam, j'ai tenté de mettre en garde mon peuple, en vain. Souillée par Ajax, enlevée par Agamemnon, je finis par trouver la mort grâce à sa femme, Clytemnestre.",
+            "Hécube : Epouse de Priam, j'ai vu ma descendance et mon peuple souffrir et être décimé, abusé sous mes propres yeux.",
+        ]
+        self.dialogue_6 = [
+            "Forcément, les plus grandes tentes appartenaient à nos chefs. Et ils ont du se partager le butin... dont les femmes des troyens.",
+            "Essayons de nous souvenir... où ont-elles été emmenées?",
+            "Polyxène-Achille: Un grand héros comme Achille se doit d'être accompagné dans la mort... mais quand même, quelle barbarie.",
+            "Cassandre-Agamemnon: La prêtresse d'Apollon a prononcé une prophetie avant d'être emmenée... un destin funeste à chacun pour le retour outre mer.",
+            "Andromaque-Neoptolème: Son bébé mort, son mari mort... Andromaque a été embarquée par Neoptolème... Je crois qu'il voulait l'épouser.",
+            "Hécube-Ulysse: Pauvre Hécube, après avoir perdu toute sa famille elle est partie à bord du navire d'Ulysse.",
+            "Tiens, les cendres ont l'air encore chaudes. Peut-être que je me suis évanoui moins longtemps que je pensais... Mais peut-être qu'ils viennent de partir tous. Il faut que je fonce à la plage, les bateaux sont peut-être encore là."
+        ]
+        self.dialogue_7_1 = [
+            "Bon je commence à y voir plus clair... Mais ?!? C'est le bateau des Danéens au loin ? Vite, une longue vue...",
+            "Oh mais j'entends des voix ? Une discussion entre les dieux ? Tendons l'oreille...",
+            "Athéna : Face à l'affront d'Ajax envers Cassandre dans mon temple, désormais Poséidon je veux, unie à toi, châtier ceux qui ne l'ont ni puni ni blamé.",
+            "Athéna frappa d\'un trait de foudre le navire d\'Ajax. [...] Ajax trouva refuge sur un rocher. [...] Poséidon l\'entendit, et d\'un coup de trident fit éclater le rocher. Ajax tomba dans la mer et mourut.",
+            "Les naufragés avalent par gorgées l\'eau affreuse à boire de la mer grondante ; puis, rendant l\'âme, ils sont emportés sur l\'onde. Les captives s\'abandonnent à la joie alors même qu\'elles expirent."
+        ]
+        self.dialogue_7_2 = [
+            "Mais oui je vois des navires au loin, sûrement ceux des Danaéens. Vite une longue vue...",
+            "Oh mais j'entends des voix ? Une discussion entre les dieux ? Tendons l'oreille...",
+            "Athéna : Face à l'affront d'Ajax envers Cassandre dans mon temple, désormais Poséidon je veux, unie à toi, châtier ceux qui ne l'ont ni puni ni blamé.",
+            "Athéna frappa d\'un trait de foudre le navire d\'Ajax. [...] Ajax trouva refuge sur un rocher. [...] Poséidon l\'entendit, et d\'un coup de trident fit éclater le rocher. Ajax tomba dans la mer et mourut.",
+            "Les naufragés avalent par gorgées l\'eau affreuse à boire de la mer grondante ; puis, rendant l\'âme, ils sont emportés sur l\'onde. Les captives s\'abandonnent à la joie alors même qu\'elles expirent."
+        ]
+        self.current_dialogue = self.dialogue_1
         self.current_text_number = 0
+
+
+        self.option_select = OptionsSelectEnd()
 
     
     def handle_input(self):
@@ -93,16 +136,16 @@ class Scene:
         self.filter2.update_animation()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            if not self.display_text and not self.enable_dialogue:
+            if not self.display_text and not self.enable_dialogue and not self.option_select.enabled:
                 self.player.move_up(self.map.tiles)
         if keys[pygame.K_DOWN]:
-            if not self.display_text and not self.enable_dialogue:
+            if not self.display_text and not self.enable_dialogue and not self.option_select.enabled:
                 self.player.move_down(self.map.tiles)
         if keys[pygame.K_LEFT]:
-            if not self.display_text and not self.enable_dialogue:
+            if not self.display_text and not self.enable_dialogue and not self.option_select.enabled:
                 self.player.move_left(self.map.tiles)
         if keys[pygame.K_RIGHT]:
-            if not self.display_text and not self.enable_dialogue:
+            if not self.display_text and not self.enable_dialogue and not self.option_select.enabled:
                 self.player.move_right(self.map.tiles)
         if keys[pygame.K_SPACE]:
             for tile in self.map.interactable_tiles:
@@ -129,17 +172,29 @@ class Scene:
                 
                 if event.key == pygame.K_k:
                     if self.enable_dialogue == False:
+                        self.current_dialogue = self.dialogue_2
                         self.enable_dialogue = True
                     elif self.enable_dialogue == True:
                         self.enable_dialogue = False
                         self.current_text_number = 0
+                
+                if event.key == pygame.K_l:
+                    if self.option_select.enabled and not self.enable_dialogue:
+                        self.option_select.enabled = False
+                    elif not self.enable_dialogue:
+                        self.option_select.enabled = True
 
                 if event.key == pygame.K_RIGHT:
                     if self.enable_dialogue and self.current_text_number < len(self.current_dialogue) - 1:
                         self.current_text_number += 1
+                    elif self.option_select.enabled:
+                        self.option_select.current_option = (self.option_select.current_option + 1) % 2
                     else:
                         self.enable_dialogue = False
                         self.current_text_number = 0
+                if event.key == pygame.K_LEFT:
+                    if self.option_select.enabled:
+                        self.option_select.current_option = (self.option_select.current_option - 1) % 2
         return True
     
     def render(self, window):
@@ -155,6 +210,8 @@ class Scene:
             self.show_text(window, self.text_to_display)
         elif self.enable_dialogue:
             self.show_dialogue(window)
+        elif self.option_select.enabled:
+            self.option_select.render(window)
         
 
     def show_text(self, window, text):
@@ -261,6 +318,84 @@ class Scene:
 
         pygame.draw.polygon(window, arrow_color, arrow_points)   
 
+class OptionsSelectEnd:
+    def __init__(self):
+        self.font = pygame.font.Font("AUGUSTUS.ttf", 20)   # credit: Manfred Klein, https://www.dafont.com/fr/augustus.font
+        self.text_color = (255, 255, 255)  # White text color
+        self.background_color = (139, 69, 19)  # Brown background color
+        self.dark_brown = (79, 45, 0)
+        self.enabled = False
+        self.text_to_display = "Qui sont les véritables vainqueurs de la guerre de Troie?"
+        self.option1 = "Les Grecs"
+        self.option2 = "Les Troyens"
+        self.current_option = 0
+
+    def render(self, window):
+        margin_bottom = 32
+        box_height = WINDOW_HEIGHT // 3
+        border_size = 8
+        shadow_offset = 2
+        shadow_color = (0, 0, 0)  # Black for the shadow
+
+        # Calculate the y-coordinate of the top of the dialogue box
+        box_top = WINDOW_HEIGHT - box_height - margin_bottom
+
+        # Create a rectangle for the dialogue box
+        box_width = WINDOW_WIDTH - 192
+        box_rect = pygame.Rect(0, 0, box_width, box_height)
+        box_rect.center = (WINDOW_WIDTH // 2, box_top + box_height // 2)
+
+        # Create the dialogue box surface with shadow
+        box_surface = pygame.Surface((box_width, box_height))
+        box_surface.fill(self.background_color)
+        box_surface_shadow = pygame.Surface((box_width, box_height))
+        box_surface_shadow.fill(shadow_color)
+
+        # Draw the shadow
+        window.blit(box_surface_shadow, (box_rect.x + shadow_offset, box_rect.y + shadow_offset))
+        # Draw the border
+        pygame.draw.rect(window, self.dark_brown, (box_rect.x - border_size, box_rect.y - border_size, box_width + 2 * border_size, box_height + 2 * border_size))
+        # Draw the dialogue box
+        window.blit(box_surface, box_rect)
+
+        # Render and display the text
+        text_surface = self.font.render(self.text_to_display, True, self.text_color)
+        text_surface_shadow = self.font.render(self.text_to_display, True, shadow_color)
+        
+        # Calculate the text position
+        text_x = box_rect.left + (box_width - text_surface.get_width()) // 2
+        text_y = box_rect.top + 40  # Padding from the top
+
+        # Display the shadow and the text
+        window.blit(text_surface_shadow, (text_x + shadow_offset, text_y + shadow_offset))
+        window.blit(text_surface, (text_x, text_y))
+
+        # Render the options
+        option1_surface = self.font.render(self.option1, True, self.text_color)
+        option2_surface = self.font.render(self.option2, True, self.text_color)
+        option1_surface_shadow = self.font.render(self.option1, True, shadow_color)
+        option2_surface_shadow = self.font.render(self.option2, True, shadow_color)
+
+        # Calculate the options positions
+        options_y = text_y + text_surface.get_height() + 40  # Padding below the text
+        option1_x = box_rect.left + box_width // 3 - option1_surface.get_width() // 2
+        option2_x = box_rect.left + 2 * box_width // 3 - option2_surface.get_width() // 2
+
+        # Display the shadow and the options
+        window.blit(option1_surface_shadow, (option1_x + shadow_offset, options_y + shadow_offset))
+        window.blit(option1_surface, (option1_x, options_y))
+        window.blit(option2_surface_shadow, (option2_x + shadow_offset, options_y + shadow_offset))
+        window.blit(option2_surface, (option2_x, options_y))
+
+        # Draw rectangle around the current selected option
+        if self.current_option == 0:
+            pygame.draw.rect(window, (255, 255, 255), (option1_x - 5, options_y - 5, option1_surface.get_width() + 10, option1_surface.get_height() + 10), 4)
+        elif self.current_option == 1:
+            pygame.draw.rect(window, (255, 255, 255), (option2_x - 5, options_y - 5, option2_surface.get_width() + 10, option2_surface.get_height() + 10), 4)
+
+        
+
+
 class Map:
     def __init__(self, width, height):
         self.width = width
@@ -309,8 +444,8 @@ class Filter:
         if self.enabled or self.is_animating():
             # Create a grayscale surface of the same size as the window
             gray_surface = pygame.Surface(self.window_size).convert()
-            gray_surface.fill((128, 128, 128))  # Fill with gray color
-            gray_surface.set_alpha(int(128 * self.animation_progress))  # Set transparency to 128 (50% opacity)
+            gray_surface.fill((0, 0, 0))  # Fill with gray color
+            gray_surface.set_alpha(int(255 * self.animation_progress))  # Set transparency to 128 (50% opacity)
 
             # Blit the grayscale surface onto the window
             window.blit(gray_surface, (0, 0))
